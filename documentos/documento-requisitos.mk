0f5a46156537b92a2456356ea75c6070453a5f38
eduardo.sys <eduardo.s@systextil.com.br>
2025-06-14 15:19:34 -0300

    Criando atores

---
+++ documentos/documento-requisitos.mk
@@ -0,0 +1,6 @@
+# Documento de Requisitos – Sistema de Gestão de Solicitações de Treinamentos
+
+## Atores Identificados
+- **Colaborador**: Solicita treinamentos.
+- **Gestor**: Aprova ou rejeita as solicitações.
+- **RH**: Valida as solicitações aprovadas.
